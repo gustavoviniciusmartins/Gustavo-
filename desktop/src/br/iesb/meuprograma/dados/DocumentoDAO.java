b7d922d3ecc299a18013fa3fecaace8696e133f1
vimagufa <30708483+vimagufa@users.noreply.github.com>
2017-11-05 14:13:49 -0200

    BO
    Feito o código da classe DocumentoBO

--- desktop/src/br/iesb/meuprograma/dados/DocumentoDAO.java
+++ desktop/src/br/iesb/meuprograma/dados/DocumentoDAO.java
@@ -3,8 +3,12 @@ package br.iesb.meuprograma.dados;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
 
 
 /**
@@ -55,14 +59,67 @@ public class DocumentoDAO implements DAO<Documento> {
 
     @Override
     public void excluir(Documento entidade) throws DadosException {
+        Connection conexao= ConexaoBD.getConexao();
+        String sql= "DELETE FROM Documentos WHERE numero=?";
+        
+        try {
+            PreparedStatement comando= conexao.prepareStatement (sql);
+            comando.setInt(1, entidade.getNumero());
+            comando.executeUpdate();
+            conexao.close();
+        } catch (SQLException ex) {
+            throw new DadosException("Erro ao excluir", ex);
+        }
+        
     }
 
     @Override
     public Documento consultar(int id) throws DadosException {
+        Connection conexao= ConexaoBD.getConexao();
+        String sql= "SELECT* FROM Documentos WHERE numero=?";
+        Documento documento= new Documento();
+        
+        try {
+            PreparedStatement comando= conexao.prepareStatement (sql);
+            comando.setInt(1, id);
+            ResultSet resultado= comando.executeQuery();
+            if (resultado.next()){
+                documento.setNumero(resultado.getInt(1));
+                documento.setNome(resultado.getString(2));
+                documento.setTipo(resultado.getString(3));
+                documento.setData(resultado.getInt(4));
+                documento.setDescricao(resultado.getString(5));               
+            }
+            conexao.close();
+        } catch (SQLException ex) {
+            throw new DadosException("Erro ao excluir");            
+        }
+        return documento;
     }
 
     @Override
     public List<Documento> listar() throws DadosException {
+        Connection conexao= ConexaoBD.getConexao();
+        String sql= "SELECT * FROM Documentos";
+        List<Documento> lista= new ArrayList<Documento>();
+        
+        try {
+            Statement comando= conexao.createStatement();
+            ResultSet resultado= comando.executeQuery(sql);
+            while(resultado.next()){
+                Documento documento= new Documento();
+                documento.setNumero(resultado.getInt(1));
+                documento.setNome(resultado.getString(2));
+                documento.setTipo(resultado.getString(3));
+                documento.setData(resultado.getInt(4));
+                documento.setDescricao(resultado.getString(5));
+                lista.add(documento);
+            }
+            conexao.close();
+        } catch (SQLException ex) {
+            throw new DadosException("Erro ao listar", ex);
+        }
+        return lista;
     }
     
 }
